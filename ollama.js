0000313f956fe48931c85612b06cd216b3d9bee3
tsereno <41271732+tsereno@users.noreply.github.com>
2024-02-04 18:49:28 -0800

    Update and rename index.js to ollama.js

---
+++ ollama.js
@@ -0,0 +1,16 @@
+//<script src="https://cdn.jsdelivr.net/npm/ollama-js-client/dist/browser/iife/ollama-js.global.js"></script>
+//<script type="module" src="https://cdn.jsdelivr.net/npm/ollama-js-client/dist/browser/index.js"></script>
+import { Ollama } from 'https://cdn.jsdelivr.net/npm/ollama-js-client/dist/browser/index.js';
+
+// use in the browser as a type="module" or in node with modules enabled (mjs)
+//import Ollama from "ollama-js-client";
+
+// use global in the browser with script tag
+const Ollama = window.OllamaJS;
+
+import Ollama from "ollama-js-client";
+const response = await new Ollama({
+  model: "llama2",
+  url: "http://127.0.0.1:11434",
+}).prompt("Hello my ai friend");
+console.log(response);
