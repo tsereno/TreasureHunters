5d8dda6d4046c5db347cd564bcae0cdbecdc2836
tsereno <41271732+tsereno@users.noreply.github.com>
2024-02-04 19:05:14 -0800

    Update ollama.js

--- ollama.js
+++ ollama.js
@@ -1,6 +1,6 @@
-import o from 'https://cdn.jsdelivr.net/npm/ollama-js-client/dist/browser/index.js';
+const Ollama = window.OllamaJS;
 
-const response = await new o({
+const response = await new Ollama({
   model: "tinydolphin:1.1b-v2.8-q4_1",
   url: "http://127.0.0.1:11434/api/",
 }).prompt("Hello my ai friend");
