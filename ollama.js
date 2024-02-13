1e30ad1f4d60548e08869eeb3a0027a1b796cd9e
tsereno <41271732+tsereno@users.noreply.github.com>
2024-02-12 22:07:59 -0800

    Delete ollama.js

--- ollama.js
+++
@@ -1,8 +0,0 @@
-//Hope this works
-const Ollama = window.OllamaJS;
-
-const response = await new Ollama({
-  model: "tinydolphin:1.1b-v2.8-q4_1",
-  url: "http://127.0.0.1:11434/api/",
-}).prompt("Hello my ai friend");
-console.log(response);
