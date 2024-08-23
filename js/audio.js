69270a61a5b77bc9ca9e9522ca7e7a5255e8d7c6
tsereno <41271732+tsereno@users.noreply.github.com>
2024-08-23 15:00:57 -0700

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -3,4 +3,27 @@
 export function playAudio(type) {
     console.log(`Playing ${type} audio`);
     // Implement actual audio playing logic here
+    // For example:
+    let sound;
+    switch(type) {
+        case 'gameStart':
+            sound = new Audio('audio/game-start.mp3');
+            break;
+        case 'collect':
+            sound = new Audio('audio/collect.mp3');
+            break;
+        case 'avoid':
+            sound = new Audio('audio/avoid.mp3');
+            break;
+        case 'treasureFound':
+            sound = new Audio('audio/treasure-found.mp3');
+            break;
+        case 'gameEnd':
+            sound = new Audio('audio/game-end.mp3');
+            break;
+        default:
+            console.log('Unknown audio type');
+            return;
+    }
+    sound.play().catch(e => console.error("Error playing audio:", e));
 }
